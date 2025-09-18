04d8719875c4714aaa2a4fe64eb07b3a7e890421
Toru Hisai <toru@torus.jp>
2025-09-18 16:40:04 +0900

    first commit

---
+++ config.mk
@@ -0,0 +1,15 @@
+PROJECT_NAME = Sheep
+CONFIG = Development
+#CONFIG = Shipping
+#MAP =TitleLevel+01_VillageLevel
+CULTURES = en+ja
+LEVEL_DIR = Sheep/Content/Sheep/Levels
+
+UE4DIR_WSL = /mnt/e/EpicGames/UE_5.4
+UE4DIR_MSYS = /E/UnrealEngine
+UE4DIR_LINUX = $(HOME)/local/UE_5.5
+UE4DIR_MAC = $(HOME)/src/UnrealEngine
+
+PROJECT_DIR = $(PWD)
+
+#GDRIVE_BUILD_DIRECTORY_ID = 1GHUoZHmCJnk_BZtE0lpSZ3Nin5I-Sh21
